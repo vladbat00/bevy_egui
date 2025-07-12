682ed807b9706888416f674c128a61332b2bb66e
hacknus <linus.stoeckli@unibe.ch>
2025-07-12 15:08:27 +0200

    wip

--- examples/color_test.rs
+++ examples/color_test.rs
@@ -681,6 +681,7 @@ impl TextureManager {
                 "color_test_gradient",
                 epaint::ColorImage {
                     size: [width, height],
+                    source_size: Vec2::from([width as f32, height as f32]),
                     pixels,
                 },
                 TextureOptions::LINEAR,
